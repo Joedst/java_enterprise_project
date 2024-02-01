5e2c8297aa2b7f75a2453e1978ad4c2e9f864485
joedst <joeledstrom@outlook.com>
2024-02-01 14:02:11 +0100

    Initial commit - setup for project & database connection.

---
+++ src/main/java/com/nero/java_enterprise_project/JavaEnterpriseProjectApplication.java
@@ -0,0 +1,13 @@
+package com.nero.java_enterprise_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaEnterpriseProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaEnterpriseProjectApplication.class, args);
+	}
+
+}
