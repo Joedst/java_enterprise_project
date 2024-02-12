4a78ebcf11c9159283ceefa482061cd9984a2020
joedst <joeledstrom@outlook.com>
2024-02-12 15:34:34 +0100

    Ordercontroller should be finished, will use as template for others.

--- src/main/java/com/nero/java_enterprise_project/controller/OrderController.java
+++ src/main/java/com/nero/java_enterprise_project/controller/OrderController.java
@@ -1,11 +1,14 @@
 package com.nero.java_enterprise_project.controller;
 
 import com.nero.java_enterprise_project.model.Order;
+import jakarta.persistence.Id;
 import org.springframework.beans.factory.annotation.Autowired;
 import com.nero.java_enterprise_project.service.OrderService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 
 @RestController
@@ -37,10 +40,20 @@ public class OrderController {
                 .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
+    @GetMapping
+public List<Order> getAllOrders(){
+        return orderService.getAllOrders();
+    }
+
     //getall orders
 
     //deletemapping
-
+@DeleteMapping("/{id}")
+    public ResponseEntity<Order> deleteOrder(@PathVariable Long id)
+{
+     orderService.deleteOrder(id);
+    return ResponseEntity.ok().build();
+}
 
 
 
