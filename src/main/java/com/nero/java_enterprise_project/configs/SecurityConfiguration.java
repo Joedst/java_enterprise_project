edfce4803f4a9c718a632cee8ba97663f8bb4181
joedst <joeledstrom@outlook.com>
2024-04-15 21:57:05 +0200

    Security config, squashed bugs (did not auth & did not post to db)

--- src/main/java/com/nero/java_enterprise_project/configs/SecurityConfiguration.java
+++ src/main/java/com/nero/java_enterprise_project/configs/SecurityConfiguration.java
@@ -1,14 +1,24 @@
 package com.nero.java_enterprise_project.configs;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 public class SecurityConfiguration {
+
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
@@ -22,4 +32,16 @@ public class SecurityConfiguration {
         return http.build();
     }
 
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
+
+    @Autowired
+    public void configureGlobal (AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(userDetailsService)
+        .passwordEncoder(passwordEncoder());
+    }
+
 }
+
--- src/main/java/com/nero/java_enterprise_project/service/impl/UserServiceImpl.java
+++ src/main/java/com/nero/java_enterprise_project/service/impl/UserServiceImpl.java
@@ -5,6 +5,7 @@ import com.nero.java_enterprise_project.model.User;
 import com.nero.java_enterprise_project.repository.UserRepository;
 import com.nero.java_enterprise_project.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
@@ -12,17 +13,19 @@ import java.util.Optional;
 @Service
 public class UserServiceImpl implements UserService {
 
-
+private final PasswordEncoder passwordEncoder;
     private final UserRepository userRepository;
 
 @Autowired
-    public UserServiceImpl(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserServiceImpl(PasswordEncoder passwordEncoder, UserRepository userRepository) {
+    this.passwordEncoder = passwordEncoder;
+    this.userRepository = userRepository;
     }
 
 
     @Override
     public User saveUser(User user) {
+user.setPassword(passwordEncoder.encode(user.getPassword()));
         return userRepository.save(user);
     }
 
